99b72adaf6721fc410ca6779a3230efe00bd0c2c
Márcio Koch <lobokoch@gmail.com>
2019-06-02 07:59:42 -0300

    Spring Boot package plug-in for service deploy

--- modules/client/src-gen/main/java/br/com/kerubin/api/financeiro/contaspagar/FinanceiroContasPagarConstants.java
+++ modules/client/src-gen/main/java/br/com/kerubin/api/financeiro/contaspagar/FinanceiroContasPagarConstants.java
@@ -1,6 +1,6 @@
 /**********************************************************************************************
 Code generated with MKL Plug-in version: 3.5.1
-Code generated at time stamp: 2019-06-01T15:26:20.750
+Code generated at time stamp: 2019-06-02T06:23:49.436
 Copyright: Kerubin - logokoch@gmail.com
 
 WARNING: DO NOT CHANGE THIS CODE BECAUSE THE CHANGES WILL BE LOST IN THE NEXT CODE GENERATION.
