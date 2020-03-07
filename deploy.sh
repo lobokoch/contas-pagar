b931ec56f2fe185ed488dab72c4365423183beb2
Márcio Koch <lobokoch@gmail.com>
2020-03-07 07:01:42 -0300

    Ajustes bash de install

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+
+# exit when any command fails
+set -e
+
+# keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# echo an error message before exiting
+trap 'echo "KERUBIN ERROR: \"${last_command}\" command filed with exit code $?."' EXIT
+
 echo "Starting project deploy..."
 cd modules
 
 echo "Packing and installing modules..."
-mvn clean install -DskipTests
+mvn clean install
 
 cd app
 
