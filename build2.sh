73a527a8812262fd2d760adf2e371a7e4adef668
Márcio Koch <lobokoch@gmail.com>
2020-03-07 07:18:18 -0300

    Ajustes no script de install e build

--- build2.sh
+++ build2.sh
@@ -6,7 +6,7 @@ set -e
 # keep track of the last executed command
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
-trap 'echo "KERUBIN ERROR: \"${last_command}\" command filed with exit code $?."' EXIT
+trap 'echo "KERUBIN FINISHED: last command \"${last_command}\" had exit code $?."' EXIT
 
 echo "Starting build generation..."
 cd modules
@@ -15,5 +15,3 @@ echo "Packing modules..."
 mvn clean install
 
 echo "DONE"
-
-read -p "Press any key..."
--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ set -e
 # keep track of the last executed command
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
-trap 'echo "KERUBIN ERROR: \"${last_command}\" command filed with exit code $?."' EXIT
+trap 'echo "KERUBIN FINISHED: last command \"${last_command}\" had exit code $?."' EXIT
 
 echo "Starting project deploy..."
 cd modules
